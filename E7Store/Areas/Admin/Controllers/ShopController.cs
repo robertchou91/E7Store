80e62d4f37b2e3358811a37e66a2f5715582f7db
Robert Chou <robertchou91@gmail.com>
2019-07-30 16:21:43 +0800

    Working on Categories page from admin.

--- E7Store/Areas/Admin/Controllers/ShopController.cs
+++ E7Store/Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using E7Store.Models;
+using E7Store.Models.ViewModels.Shop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,22 @@ namespace E7Store.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
         {
-            return View();
+            // declare the list of CategoryVM
+            List<CategoryVM> categoryVMList;
+
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                // Init the list
+                categoryVMList = db.Categories
+                                .ToArray()
+                                .OrderBy(x => x.Sorting)
+                                .Select(x => new CategoryVM(x))
+                                .ToList();
+            }
+            return View(categoryVMList);
         }
     }
 }
