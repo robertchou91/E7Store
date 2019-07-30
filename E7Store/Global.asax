f5da0978079ed553d64cdf818ba1f3d378039385
robertchou91 <robertchou91@gmail.com>
2019-07-30 13:13:48 +0800

    first commit

---
+++ E7Store/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E7Store.MvcApplication" Language="C#" %>
---
+++ E7Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E7Store.Startup))]
+namespace E7Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
