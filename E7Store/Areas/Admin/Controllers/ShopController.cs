92946be651f75be430a3ce6dafac84625a820c1a
Robert Chou <robertchou91@gmail.com>
2019-07-31 13:46:13 +0800

    Work on Categories Methods in Admin Page.

--- E7Store/Areas/Admin/Controllers/ShopController.cs
+++ E7Store/Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
 ﻿using E7Store.Models;
+using E7Store.Models.Data;
 using E7Store.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
@@ -27,6 +28,109 @@ namespace E7Store.Areas.Admin.Controllers
             }
             return View(categoryVMList);
         }
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string categoryName)
+        {
+            // Declare the id
+            string id;
+
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                // check if category name is unique
+                if (db.Categories.Any(x => x.Name == categoryName))
+                    return "titletaken";
+
+                // init data
+                var data = new Category();
+
+                // add to data
+                data.Name = categoryName;
+                data.Slug = categoryName.Replace(" ", "-").ToLower();
+                data.Sorting = 100;
+
+                db.Categories.Add(data);
+                // save data
+                db.SaveChanges();
+
+                //get the id
+                id = data.Id.ToString();
+            }
+
+            // return the id
+            return id;
+        }
+
+        // POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                // set initial count
+                int count = 1;
+
+                // declare Category
+                Category data;
+
+                // set sorting for each category
+                foreach (var categoryId in id)
+                {
+                    data = db.Categories.Find(categoryId);
+                    data.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+        // DELETE: Admin/Shop/DeleteCategory/id
+        public ActionResult DeleteCategory(int id)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                // get the page
+                Category data = db.Categories.Find(id);
+
+                // remove the page
+                db.Categories.Remove(data);
+
+                // Save
+                db.SaveChanges();
+            }
+
+            // Redirect
+            return RedirectToAction("Categories");
+        }
+
+        // POST: Admin/Shop/RenameCategory
+        [HttpPost]
+        public string RenameCategory(string newCategoryName, int id)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                // check if category name is unique
+                if (db.Categories.Any(x => x.Name == newCategoryName))
+                {
+                    return "titletaken";
+                }
+
+                // get the data
+                Category data = db.Categories.Find(id);
+
+                // edit data
+                data.Name = newCategoryName;
+                data.Slug = newCategoryName.Replace(" ", "-").ToLower();
+
+                // save changes
+                db.SaveChanges();
+            }
+
+            // return 
+            return "ok";
+        }
+
     }
 }
 
